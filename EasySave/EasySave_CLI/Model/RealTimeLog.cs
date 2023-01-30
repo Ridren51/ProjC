0ba0f07540e0240f243f4deb14bff1469bc67702
Manissar <boutikarnassim@gmail.com>
2023-01-30 22:58:20 +0100

    Log classes and interfaces

---
+++ EasySave/EasySave_CLI/Model/RealTimeLog.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasySave_CLI.Model
+{
+    internal class RealTimeLog : IRealTimeLog
+    {
+        public bool State 
+        {
+            get { return State; }
+            set { State = value; }
+        }
+
+        public void createLog()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void updateLog()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
