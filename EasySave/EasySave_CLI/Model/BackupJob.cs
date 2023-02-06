97b278698e5df7d401e07d99b31d483d17ba8c46
Ridren51 <clementruzicka@gmail.com>
2023-02-06 09:02:42 +0100

    feat(Backup): initial commit backup

--- EasySave/EasySave_CLI/Model/BackupJob.cs
+++ EasySave/EasySave_CLI/Model/BackupJob.cs
@@ -1,14 +1,46 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-/// <summary>
-/// Summary description for Class1
-/// </summary>
-public class Class1
+namespace EasySave_CLI.Model
 {
-	public Class1()
-	{
-		//
-		// TODO: Add constructor logic here
-		//
-	}
+    internal class BacckupJob
+    {
+        string Name { get; set; }
+        bool State { get; set; }
+        string SourceDirectory { get; set; }
+        string TargetDirectory { get; set; }
+        string Type { get; set; }
+        DateTime Date { get; set; }
+
+        public void doBackupJob()
+        {
+            try
+            {
+                // Combiner les chemins d'accès pour obtenir le chemin complet du fichier source
+                string SourceFile = Path.Combine(SourceDirectory, Name);
+
+                // Combiner les chemins d'accès et ajouter la date et le type de sauvegarde pour obtenir le chemin complet du fichier cible
+                string TargetFile = Path.Combine(TargetDirectory, Type + "_" + DateTime.Now.ToString("yyyyMMdd") + "_" + Name);
+
+                // Copier le fichier de la source vers la cible
+                File.Copy(SourceFile, TargetFile, true);
+
+                Console.WriteLine("Le fichier a été sauvegardé avec succès.");
+            }
+            catch (IOException ex)
+            {
+                Console.WriteLine("Une erreur s'est produite lors de la sauvegarde du fichier : " + ex.Message);
+            }
+        }
+        public void getBackupVersion()
+        {
+
+        }
+    }
 }
+
+
