5b9298fc5f386a1d35e854192ae5e0394de2a07c
Ridren51 <clementruzicka@gmail.com>
2023-02-06 19:10:41 +0100

    feat(BackupJob): initial doBackupJob

--- EasySave/EasySave_CLI/Program.cs
+++ EasySave/EasySave_CLI/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using EasySave_CLI.Model;
+using System;
+
+BackupJob init = new BackupJob();
+init.doBackupJob();
