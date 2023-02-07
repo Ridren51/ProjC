8f49f3df7251ee5bc3a5b8958a1dfbc76b03a809
Manissar <boutikarnassim@gmail.com>
2023-02-07 21:52:30 +0100

    feat(View): View binded with adapter and displaying all the information necessary for the project, and use adapter to make the tasks

--- EasySave/EasySave_CLI/View/View.cs
+++ EasySave/EasySave_CLI/View/View.cs
@@ -1,4 +1,6 @@
-﻿using EasySave_CLI.ViewModel;
+﻿using EasySave_CLI.Model;
+using EasySave_CLI.ViewModel;
+using Microsoft.VisualBasic.FileIO;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,8 +12,10 @@ namespace EasySave_CLI.View
     public class View
     {
         private Adapter _adapter;
+        private ConsoleLanguage _consoleLanguage;
         public View() {
             _adapter = new Adapter();
+            _consoleLanguage = _adapter.ConsoleLanguage;
         }
 
         public void Start()
@@ -19,8 +23,8 @@ namespace EasySave_CLI.View
             ShowHelp();
             while (true)
             {
-                Console.WriteLine("Please choose an option: ");
-                string option = Console.ReadLine();
+                Console.WriteLine(_consoleLanguage.GetString("Option"));
+                string? option = Console.ReadLine();
                 if (int.TryParse(option, out int value))
                     ParseInput(value);
             }
@@ -31,39 +35,79 @@ namespace EasySave_CLI.View
             switch (option)
             {
                 case 1:
-                    Console.Clear();
-                    ShowHelp();
-                    break;
+                    Console.WriteLine("1"); break;
                 case 2:
                     Console.WriteLine("2"); break;
                 case 3:
-                    Console.WriteLine("3"); break;
+                    AddBackupJob(); break;
                 case 4:
-                    Console.WriteLine("4"); break;
+                    DeleteBackupJob(); break;
                 case 5:
-                    Console.WriteLine("5"); break;
+                    this.ShowBackupQueue(); break;
                 case 6:
-                    Console.WriteLine("6"); break;
+                    Console.WriteLine("Not implemented yet"); break;
                 case 7:
-                    break;
+                    SetLanguage(); break;
                 case 8:
                     System.Environment.Exit(0); break;
                 default:
-                    ShowHelp(); break;
+                    Console.Clear(); ShowHelp(); break;
             }
         }
         private void ShowHelp()
         {
             Console.WriteLine("\r ___  __    ____   __ __   __   _   _  ___  \r\n| __|/  \\ /' _| `v' /' _/ /  \\ | \\ / || __| \r\n| _|| /\\ |`._`.`. .'`._`.| /\\ |`\\ V /'| _|  \r\n|___|_||_||___/ !_! |___/|_||_|  \\_/  |___| \n");
-            Console.WriteLine("1- Run backups");
-            Console.WriteLine("2- Run specific backup");
-            Console.WriteLine("3- Add backup job");
-            Console.WriteLine("4- Remove backup job");
-            Console.WriteLine("5- Show backup queue");
-            Console.WriteLine("6- Restore backup");
-            Console.WriteLine("7- Choose language");
-            Console.WriteLine("8- exit");
+            Console.WriteLine(_consoleLanguage.GetString("HelpText"));
 
+        }
+
+        private void SetLanguage()
+        {
+            Console.WriteLine("1- English");
+            Console.WriteLine("2- Francais");
+            if (int.TryParse(Console.ReadLine(), out int language))
+            {
+                switch(language)
+                {
+                    case 1: _adapter.SetLanguage(_adapter.GetEnglishLanguage()); break;
+                    case 2: _adapter.SetLanguage(_adapter.GetFrenchLanguage()); break;
+
+                }
+                Console.Clear();
+                ShowHelp();
+            }
+
+        }
+
+        private void AddBackupJob()
+        {
+            if (_adapter.IsBackupQueueFull()) {
+                Console.WriteLine(_consoleLanguage.GetString("QueueFull"));
+                return;
+             }
+            Console.WriteLine(_consoleLanguage.GetString("EnterName"));
+            string name = Console.ReadLine();
+            if (_adapter.IsNameValid(name))
+                _adapter.AddBackupJob(name);
+            else
+                Console.WriteLine(_consoleLanguage.GetString("InvalidName"));
+        }
+
+        private void DeleteBackupJob()
+        {
+            ShowBackupQueue();
+            Console.WriteLine(_consoleLanguage.GetString("EnterIndex"));
+            if (int.TryParse(Console.ReadLine(), out int index))
+                _adapter.RemoveBackupJob(index - 1);
+            else
+                Console.WriteLine(_consoleLanguage.GetString("EnterIndex"));
+        }
+
+        private void ShowBackupQueue()
+        {
+            for (int i = 0; i < _adapter.BackupJobs.Count; i++) {
+                Console.WriteLine(i + 1 + "- " + _adapter.GetBackupJob(i).ToString());
+             }
         }
     }
 }
