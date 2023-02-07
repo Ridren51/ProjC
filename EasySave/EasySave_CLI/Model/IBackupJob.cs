c227eb7cf9a6a39b3e18710f5e86e1c5502689bd
Ridren51 <clementruzicka@gmail.com>
2023-02-07 14:25:35 +0100

    feat(IBackupJob): intial commit Interface BackupJob

---
+++ EasySave/EasySave_CLI/Model/IBackupJob.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasySave_CLI.Model
+{
+    internal interface IBackupJob
+    {
+      public void doBackup(string SourceDirectory, string TargetDirectory);
+      public void doDifferiencialBackup(string SourceDirectory, string TargetDirectory, int delayInHour);
+    }
+}
