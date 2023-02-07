77e94bc3b3e728d893792c46f675d7d3efd5753f
Manissar <boutikarnassim@gmail.com>
2023-02-07 23:24:26 +0100

    feat(backup): enum for backup type

---
+++ EasySave/EasySave_CLI/Model/BackupEnum.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasySave_CLI.Model
+{
+    internal enum BackupEnum
+    {
+        [System.ComponentModel.Description("diff")]
+        Differential = 1,
+        [System.ComponentModel.Description("stand")]
+        Standard = 2,
+    }
+}
