2fefe5563dac0ba60666599e7eb7770514e2825c
Manissar <boutikarnassim@gmail.com>
2023-02-20 09:30:22 +0100

    feat(log): added XML format refactor(log): using delegate to choose dynamically between JSON or XML format feat(log): added LogType and LogFormat Enum feat(log): LogFactory class to get the subclass of Log refactor(log): using LogFactory for the log type instead of raw condition

--- EasySave/EasySave_CLI/Model/Logs/Log.cs
+++ EasySave/EasySave_CLI/Model/Logs/Log.cs
@@ -4,14 +4,14 @@ using System.Linq;
 using System.Text;
 using System.Text.Json;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace EasySave_CLI.Model.Logs
 {
     public abstract class Log : ILog
     {
         protected string _logPath = "";
-        public abstract Type LogType { get; }
-
+        protected delegate string GetLogDelegate(ITransferFile file);
         public abstract void UpdateLog(ITransferFile file);
 
         protected void CreateLogFile()
@@ -23,11 +23,50 @@ namespace EasySave_CLI.Model.Logs
         }
         protected string getLogJSON(ITransferFile file)
         {
-            JsonLog jsonLog;
-            jsonLog = LogType == typeof(RealTimeLog) ? jsonLog = new JsonLog(file, (RealTimeLog)this) : jsonLog = new JsonLog(file, (DailyLog)this);
-            JsonSerializerOptions options = new JsonSerializerOptions { WriteIndented = true, DefaultIgnoreCondition = System.Text.Json.Serialization.JsonIgnoreCondition.WhenWritingNull };
+            LogType logType = LogFactory.getLogType(this);
+            JsonLog jsonLog = logType switch
+            {
+                LogType.RealTimeLog => new JsonLog(file, (RealTimeLog)this),
+                LogType.DailyLog => new JsonLog(file, (DailyLog)this),
+                _ => throw new ArgumentException($"Invalid LogType: {logType}"),
+            };
+            JsonSerializerOptions options = new JsonSerializerOptions{
+                WriteIndented = true,
+                DefaultIgnoreCondition = System.Text.Json.Serialization.JsonIgnoreCondition.WhenWritingNull
+            };
             string jsonString = JsonSerializer.Serialize(jsonLog, options);
             return jsonString;
+        }
+        protected string getLogXML(ITransferFile file)
+        {
+
+            var jsonDocument = JsonDocument.Parse(getLogJSON(file));
+            var jsonRoot = jsonDocument.RootElement;
+
+            XElement xml = new XElement("RealTimeLog");
+
+            foreach (var property in jsonRoot.EnumerateObject())
+            {
+                xml.Add(new XElement(property.Name, property.Value.ToString()));
+            }
+            return xml.ToString();
+        }
+
+        protected string getLog(ITransferFile file, LogFormat logFormat)
+        {
+            GetLogDelegate getLogDelegate = null;
+            switch (logFormat)
+            {
+                case LogFormat.JSON:
+                    getLogDelegate = getLogJSON;
+                    break;
+                case LogFormat.XML:
+                    getLogDelegate = getLogXML;
+                    break;
+                default:
+                    throw new ArgumentException($"Invalid log format: {logFormat}");
+            }
+            return getLogDelegate(file);
         }
     }
 
