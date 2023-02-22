d9af2e8593ee2317545f168e403326650a4a2b37
Ridren51 <clementruzicka@gmail.com>
2023-02-22 16:55:31 +0100

    feat(ViewModel):adapterWPF

---
+++ EasySave/AppCore/ViewModel/WPFAdapter.cs
@@ -0,0 +1,25 @@
+﻿using AppCore.Model.Backup;
+using AppCore.Model.Utilities;
+using EasySave_WPF;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using AppCore.Model;
+
+
+namespace EasySave_WPF.ViewModel
+{
+    public class WPFAdapter
+    {
+        public List<BackupJob> BackupJobs { get; set; }
+        public WPFAdapter()
+        {
+        }
+    }
+}
