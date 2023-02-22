b055311149583f98203543b6b6a66df156eae9a7
Ridren51 <clementruzicka@gmail.com>
2023-02-22 16:56:10 +0100

    feat(WPF): WPF View Update

--- EasySave/EasySave_WPF/MainWindow.axaml.cs
+++ EasySave/EasySave_WPF/MainWindow.axaml.cs
@@ -1,4 +1,11 @@
 using Avalonia.Controls;
+using AppCore.Model;
+using EasySave_WPF.ViewModel;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+using Avalonia.Data;
+using System.Collections.ObjectModel;
 
 namespace EasySave_WPF;
 
